a22108e9cc51f782ba2acfa0dd6c76750093d755
Diogo Castro <dcastro@spotx.tv>
2017-12-29 19:37:45 +0000

    Call `stack query` and decode YAML result

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,31 +1,67 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-
-
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib where
 
--- import Stack.Build
--- import Stack.Types.Config
--- import Stack.Runners
--- import Stack.Options.GlobalParser
-import System.Process
-import Data.Yaml
-
+import System.Process (readCreateProcessWithExitCode, proc, cwd)
+import GHC.Generics (Generic)
+import Data.Yaml (FromJSON, Value(..), (.:))
+import qualified Data.Yaml as Y
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.HashMap.Strict as HM
+import Data.Text (Text)
+import System.Exit (die, ExitCode(..))
 
 stackDir, wd :: String
 stackDir = "/Users/dcastro/.local/bin/stack"
+-- wd = "/Users/dcastro/Dropbox/Projects/Haskell/halive"
 wd = "/Users/dcastro/Dropbox/Projects/Haskell/haskell-ide-engine"
 
-stackQuery :: IO String
-stackQuery = readCreateProcess process ""
+stackQuery :: IO [Component]
+stackQuery = 
+  do
+    (exitCode, stdout, stderr) <- readCreateProcessWithExitCode process ""
+    
+    case exitCode of
+      ExitFailure _ -> die ("`stack query` failed:\n" ++ stderr)
+      ExitSuccess   -> pure ()
+
+    case decodeComponents stdout of
+      Right cs -> pure cs
+      Left err -> die ("Could not parse response from `stack query`:\n" ++ err)
   where
     process = (proc stackDir ["query"]) { cwd = Just wd }
 
--- stackQuery :: IO ()
--- stackQuery = queryCmd [] (globalOptsFromMonoid False mempty)
---   where
---     queryCmd selectors go = withBuildConfig go $ queryBuildInfo $ map T.pack selectors
+
+decodeComponents :: String -> Either String [Component]
+decodeComponents s = 
+  let resultOpt = Y.decodeEither (BS.pack s) :: Either String Result
+  in  components . locals <$> resultOpt
+
+newtype Result = Result
+  { locals :: Locals
+  }
+  deriving (Generic, Show)
+
+newtype Locals = Locals { components :: [Component] }
+  deriving Show
+
+data Component = Component
+  { name :: Text
+  , path :: Text
+  , version :: Text
+  }
+  deriving Show
+
+instance FromJSON Result
+
+instance FromJSON Locals where
+  parseJSON (Object o) = 
+    let pairs = HM.toList o
+        parseComponent (k, Object v) = Component k <$> v .: "path" <*> v .: "version"
+    in  Locals <$> traverse parseComponent pairs
+  parseJSON _ = fail "Expected Object for Locals value"
+
 
 
 
